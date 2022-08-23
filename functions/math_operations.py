028ad51721e7c37ea5c9fdf5571cdb9e39c26a4f
victord03 <vic.kaklamanis@gmail.com>
2022-08-23 22:42:15 +0300

    Functional map() call.

---
+++ functions/math_operations.py
@@ -0,0 +1,9 @@
+from math import sqrt
+
+
+def square_root(a: float) -> float:
+    return round(sqrt(a), 2)
+
+
+def add_two_numbers(a: int, b: int) -> int:
+    return a + b
--- src/main.py
+++ src/main.py
@@ -1,7 +1,18 @@
+from functions import math_operations as mo
+from random import randint as rnd
 
 
 def main():
-    ...
+
+    list_of_numbers = [rnd(1, 29) for _ in range(0, 9)]
+    # list_of_tuples = [(rnd(0, 10), rnd(21, 39)) for _ in range(0, 9)]
+
+    a = list(map(mo.square_root, list_of_numbers))
+
+    for i in range(0, len(list_of_numbers)):
+        print(
+            f"\nsqrt({list_of_numbers[i]}) ~ {a[i]}"
+        )
 
 
 if __name__ == "__main__":
